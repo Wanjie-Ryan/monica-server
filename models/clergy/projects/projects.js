dfad9a1f563b5383ec4089a3eeae7cb2aad8a86a
Ryan <ryanwanjiedistortion@gmail.com>
2023-08-02 18:21:41 +0300

    monica-server

---
+++ models/clergy/projects/projects.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const projectSchema = new mongoose.Schema({
+
+    title:{
+
+        type:String,
+        required:[true, 'Provide the name of the Project']
+    },
+
+    image:{
+
+        type:String,
+        required:[true, 'Provide the image of the Project']
+
+    },
+
+    description:{
+
+        type:String,
+        required:[true, 'Provide a brief description of the Project']
+
+    }
+
+
+
+})
+
+
+
+module.exports = mongoose.model('Project', projectSchema)
