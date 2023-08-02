6893fe96773165d26090f145150b553ff953a9c6
Ryan <ryanwanjiedistortion@gmail.com>
2023-08-02 19:30:13 +0300

    monica-server

---
+++ controllers/clergy/Events/GE/ge.js
@@ -0,0 +1,30 @@
+const Events = require('../../../../models/Events/events')
+const {StatusCodes} = require('http-status-codes')
+
+
+const CreateEvents = (req,res)=>{
+
+    res.send('hey')
+}
+
+const GetAllEvents = (req,res)=>{
+
+    res.send('hey')
+}
+
+const GetSingleEvents = (req,res)=>{
+
+    res.send('hey')
+}
+
+const UpdateEvents = (req,res)=>{
+
+    res.send('hey')
+}
+
+const DeleteEvents = (req,res)=>{
+
+    res.send('hey')
+}
+
+module.exports = {CreateEvents, GetAllEvents, GetSingleEvents, UpdateEvents, DeleteEvents}
