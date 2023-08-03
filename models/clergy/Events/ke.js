4b0adf860164abb70880c9afee5ec67c24269dd5
Ryan <ryanwanjiedistortion@gmail.com>
2023-08-03 07:17:35 +0300

    monica-server

---
+++ models/clergy/Events/ke.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const KeventSchema = new mongoose.Schema({
+
+    title:{
+
+        type:String,
+        required:[true, 'Provide the name of the Event']
+    },
+
+    image:{
+
+        type:String,
+        required:[true, 'Provide the image of the Event']
+
+    },
+
+    ActualDate:{
+
+        //YYYY-MM-DD
+        type:Date,
+        required:[true, 'Provide the Actual Date for the Event']
+    },
+
+    DeadlineDate:{
+
+        type:Date,
+        required:[true, 'Provide the Deadline Date for the Event']
+    },
+
+    description:{
+
+        type:String,
+        required:[true, 'Provide a brief description of the Event'],
+        maxlength:50
+
+    }
+
+
+
+})
+
+
+
+module.exports = mongoose.model('KEvents', KeventSchema)
