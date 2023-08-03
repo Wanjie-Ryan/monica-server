3f0f2cc1f3bfd76b4e34d19e5654b077aff7b49a
Ryan <ryanwanjiedistortion@gmail.com>
2023-08-03 10:21:18 +0300

    monica-server

--- models/user/feedbacks/feedback.js
+++ models/user/feedbacks/feedback.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 
 const feedbackSchema = new mongoose.Schema({
@@ -12,7 +13,13 @@ const feedbackSchema = new mongoose.Schema({
     email:{
 
         type:String,
-        required:[true, 'Provide the email']
+        required:[true, 'Provide the email'],
+        match:[
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please provide a valid email',
+            
+            ], 
+
+        validate: [validator.isEmail, 'Please provide a valid email'],
     },
 
     message:{
