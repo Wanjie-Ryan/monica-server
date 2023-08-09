801a8bd8ee6de9cd8918f11a7dd2366c433f4f75
Ryan <ryanwanjiedistortion@gmail.com>
2023-08-09 21:01:10 +0300

    clergy-integration

--- routes/clergy/Events/GE/ge.js
+++ routes/clergy/Events/GE/ge.js
@@ -1,17 +1,20 @@
-const express = require('express')
-const router = express.Router()
-const {CreateEvents, GetAllEvents, GetSingleEvents, UpdateEvents, DeleteEvents,SearchEvents} = require('../../../../controllers/clergy/Events/GE/ge')
+const express = require("express");
+const router = express.Router();
+const {
+  CreateEvents,
+  GetAllEvents,
+  GetSingleEvents,
+  UpdateEvents,
+  DeleteEvents,
+  SearchEvents,
+} = require("../../../../controllers/clergy/Events/GE/ge");
 const authMiddleware = require("../../../../middleware/auth");
 
+router.route("/createevent").post(authMiddleware, CreateEvents);
+router.route("/allevents").get(GetAllEvents);
+router.route("/singleevent/:id").get(GetSingleEvents);
+router.route("/updateevent/:id").patch(authMiddleware, UpdateEvents);
+router.route("/deleteevent/:id").delete(authMiddleware, DeleteEvents);
+router.route("/searchevent").get(SearchEvents);
 
-
-router.route('/createevent').post(authMiddleware,CreateEvents)
-router.route('/allevents').get(GetAllEvents)
-router.route('/singleevent/:id').get(GetSingleEvents)
-router.route('/updateevent/:id').patch(authMiddleware,UpdateEvents)
-router.route('/deleteevent/:id').delete(authMiddleware,DeleteEvents)
-router.route('/searchevent').get(SearchEvents)
-
-module.exports = router
-
-
+module.exports = router;
